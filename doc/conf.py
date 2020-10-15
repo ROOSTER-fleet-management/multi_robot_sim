9b116687b531916f4a5526233b0820cf761e54c3
Neel Nagda <neel.nagda1@gmail.com>
2020-10-15 12:30:30 +0200

    added scripts folder to python path so autodoc can find the scripts

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,8 @@ import os
 import catkin_pkg.package
 catkin_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
 catkin_package = catkin_pkg.package.parse_package(os.path.join(catkin_dir, catkin_pkg.package.PACKAGE_MANIFEST_FILENAME))
-sys.path.insert(0, os.path.abspath('.'))
+import sys
+sys.path.insert(0, os.path.abspath('../scripts'))
 
 # -- Project information -----------------------------------------------------
 
